2ab94df3b2fafc1110bbd5a259822adce6ab0dd1
p_xyunli <p_xyunli@tencent.com>
2023-12-18 23:02:26 +0800

    场景检查

--- packages/webgal/src/Core/controller/scene/sceneFetcher.ts
+++ packages/webgal/src/Core/controller/scene/sceneFetcher.ts
@@ -4,9 +4,23 @@ import axios from 'axios';
  * 原始场景文件获取函数
  * @param sceneUrl 场景文件路径
  */
-export const sceneFetcher = (sceneUrl: string) => {
+export const sceneFetcher = async (sceneUrl: string) => {
+  let url = sceneUrl;
+  let checkUrl = 'http://localhost:8080/api/v1/game/scene?url=' + sceneUrl;
+  // sceneUrl 如果结果为404，则使用默认场景 end.txt
+  try {
+    await axios.get(url);
+    // 发送get 请求
+    const resp = await axios.get(checkUrl);
+    if (resp.data.message !== 'success') {
+      url = './game/scene/end.txt';
+    }
+  } catch (error) {
+    url = './game/scene/end.txt';
+  }
+  console.log('sceneUrl', url);
   return new Promise<string>((resolve) => {
-    axios.get(sceneUrl).then((response) => {
+    axios.get(url).then((response) => {
       const rawScene: string = response.data.toString();
       resolve(rawScene);
     });
